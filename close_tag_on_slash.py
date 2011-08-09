7334785a4e2fa685fb1f71336dbd5c4241c36784
Mattias Kihlstrom <mattias@kihlstrom.com>
2011-08-09 23:27:06 +0200

    Added a license (ISC) and the first version of the command class. Added installation instructions to README.

---
+++ close_tag_on_slash.py
@@ -0,0 +1,55 @@
+import sublime, sublime_plugin
+
+class CloseTagOnSlashCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.view.insert(edit, self.view.sel()[0].begin(), '/')
+		before = self.view.substr(sublime.Region(0, self.view.sel()[0].begin()))
+		if '<' == before[-2]:
+			before = self.removeComments(before[0:-2])
+			tag = None
+			while (tag is None) and (before is not None):
+				before, after = self.splitOnNearestClosedTag(before)
+				tag = self.nearestUnclosedTag(after)
+			if tag is not None:
+				self.view.insert(edit, self.view.sel()[0].begin(), tag + '>')
+
+	def removeComments(self, string):
+		commentStart = string.find('<!--')
+		while -1 != commentStart:
+			commentEnd = string.find('-->', commentStart + 4)
+			if -1 == commentEnd:
+				return string
+			string = string[0:commentStart] + string[commentEnd + 3:]
+			commentStart = string.find('<!--')
+		return string
+
+	def splitOnNearestClosedTag(self, after):
+		before = None
+		closeStart = after.rfind('</')
+		if -1 != closeStart:
+			closeEnd = after.find('>', closeStart + 2)
+			if -1 != closeEnd:
+				tag = after[closeStart + 2:closeEnd]
+				before = after[0:closeStart]
+				openStart = before.rfind('<' + tag + '>')
+				if -1 == openStart:
+					openStart = before.rfind('<' + tag + ' ')
+				if -1 != openStart:
+					before = before[0:openStart]
+				after = after[closeEnd + 1:]
+		return before, after
+
+	def nearestUnclosedTag( self, string ) :
+		openStart = string.rfind('<')
+		if -1 != openStart:
+			openEnd = string.find('>', openStart + 1)
+			if '/' == string[openEnd - 1]:
+				if 0 == openStart:
+					return None
+				else:
+					return self.nearestUnclosedTag(string[0:openStart - 1])
+			else:
+				space = string.find(' ', openStart + 1)
+				if -1 != space:
+					openEnd = space
+				return string[openStart + 1:openEnd]
