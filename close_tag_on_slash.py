3db937e50b2222a68dafe5d3151edbe1126516d1
Tito Bouzout <tito.bouzout@gmail.com>
2011-10-06 03:54:45 -0300

    improves: works with multiple selection fix: when trying to insert / when there is multiples regions, only the first region gets the character "/"

--- close_tag_on_slash.py
+++ close_tag_on_slash.py
@@ -2,12 +2,15 @@ import sublime, sublime_plugin
 
 class CloseTagOnSlashCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		cursorPosition = self.view.sel()[0].begin()
-		previousCharacter = self.view.substr(sublime.Region(cursorPosition - 1, cursorPosition))
-		if '<' == previousCharacter:
-			self.view.erase(edit, sublime.Region(cursorPosition - 1, cursorPosition))
-			self.view.run_command('close_tag')
-			if self.view.sel()[0].begin() < cursorPosition:
-				self.view.insert(edit, cursorPosition - 1, '</')
-		else:
-			self.view.insert(edit, cursorPosition, '/')
+
+		closeTags = False;
+		for region in self.view.sel():
+			cursorPosition = region.begin()
+			previousCharacter = self.view.substr(sublime.Region(cursorPosition - 1, cursorPosition))
+			if '<' == previousCharacter:
+				self.view.erase(edit, sublime.Region(cursorPosition - 1, cursorPosition))
+				closeTags = True;
+			else:
+				self.view.insert(edit, cursorPosition, '/');
+		if closeTags:
+			self.view.run_command('close_tag');
