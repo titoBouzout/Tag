bec4b18209a98c7cd297a3b27508d8fd547f3282
Roberto Bouzout (tito) <titodp@gmail.com>
2011-10-08 16:13:57 -0300

    fix: when something is selected "/" is not inserted. improves: add scope html to prevent the command to run on unrelated languages.

--- close_tag_on_slash.py
+++ close_tag_on_slash.py
@@ -2,15 +2,18 @@ import sublime, sublime_plugin
 
 class CloseTagOnSlashCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-
 		closeTags = False;
 		for region in self.view.sel():
 			cursorPosition = region.begin()
 			previousCharacter = self.view.substr(sublime.Region(cursorPosition - 1, cursorPosition))
-			if '<' == previousCharacter:
+			if '<' == previousCharacter and self.view.score_selector(cursorPosition, 'text.html') > 0:
 				self.view.erase(edit, sublime.Region(cursorPosition - 1, cursorPosition))
 				closeTags = True;
 			else:
-				self.view.insert(edit, cursorPosition, '/');
+				if region.empty():
+					self.view.insert(edit, cursorPosition, '/');
+				else:
+					self.view.erase(edit, sublime.Region(region.begin(), region.end()))
+					self.view.insert(edit, region.begin(), '/');
 		if closeTags:
 			self.view.run_command('close_tag');
