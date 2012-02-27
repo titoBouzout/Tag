9d6d3b2fda5a650266188db400d667644ee7df72
Tito Bouzout <tito.bouzout@gmail.com>
2012-02-27 12:35:27 -0300

    Move activation logic into keymap ( thanks spadgos )

--- tag_close_tag_on_slash.py
+++ tag_close_tag_on_slash.py
@@ -3,28 +3,29 @@ import sublime, sublime_plugin, re
 class TagCloseTagOnSlashCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		closeTags = False;
-		for region in self.view.sel():
+		view = self.view
+		for region in view.sel():
 			cursorPosition = region.begin()
-			previousCharacter = self.view.substr(sublime.Region(cursorPosition - 1, cursorPosition))
+			previousCharacter = view.substr(sublime.Region(cursorPosition - 1, cursorPosition))
 
-			if '<' == previousCharacter and self.view.score_selector(cursorPosition, 'text.html | text.xml') > 0:
-				syntax = self.view.syntax_name(region.begin())
+			if '<' == previousCharacter and view.score_selector(cursorPosition, 'text.html | text.xml') > 0:
+				syntax = view.syntax_name(region.begin())
 				should_skip = re.match(".*string.quoted.double", syntax) or re.match(".*string.quoted.single", syntax)
 				if not should_skip:
-					self.view.erase(edit, sublime.Region(cursorPosition - 1, cursorPosition))
+					view.erase(edit, sublime.Region(cursorPosition - 1, cursorPosition))
 					closeTags = True;
 				else:
 					if region.empty():
-						self.view.insert(edit, cursorPosition, '/');
+						view.insert(edit, cursorPosition, '/');
 					else:
-						self.view.replace(edit, sublime.Region(region.begin(), region.end()), '/');
+						view.replace(edit, sublime.Region(region.begin(), region.end()), '/');
 			else:
 				if region.empty():
-					self.view.insert(edit, cursorPosition, '/');
+					view.insert(edit, cursorPosition, '/');
 				else:
-					self.view.replace(edit, sublime.Region(region.begin(), region.end()), '/');
+					view.replace(edit, sublime.Region(region.begin(), region.end()), '/');
 		if closeTags:
-			self.view.run_command('close_tag');
-			if self.view.window():
-				self.view.window().show_input_panel("boo!", '', '', None, None)
-				self.view.window().run_command('hide_panel');
+			view.run_command('close_tag');
+			if view.window():
+				view.window().show_input_panel("boo!", '', '', None, None)
+				view.window().run_command('hide_panel');
