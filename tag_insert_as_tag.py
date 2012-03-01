2f95c3ecd4447d56bd5f1a9d11e9df55439eb419
Tito Bouzout <tito.bouzout@gmail.com>
2012-03-01 13:30:10 -0300

    exepction in insert as tag.

--- tag_insert_as_tag.py
+++ tag_insert_as_tag.py
@@ -4,25 +4,28 @@ import re
 
 class TagInsertAsTagCommand(sublime_plugin.TextCommand):
 
-  def run(self, edit):
-    view = self.view
-    self_closing_tags = re.compile("^(area|base|br|col|frame|hr|img|input|link|meta|param|command|embed|source)", re.I)
-    new_selections = []
-    for region in view.sel():
-      if view.score_selector(region.a, 'text.html, text.xml') > 0:
-        source = view.substr(region)
-        if not source.strip():
-          pass
-        elif re.match("^\s", source):
-          view.replace(edit, region, '<p>'+source+'</p>')
-          new_selections.append(sublime.Region(region.end()+3, region.end()+3))
-        elif self_closing_tags.match(source):
-          view.replace(edit, region, '<'+source+'/>')
-          new_selections.append(sublime.Region(region.end()+3, region.end()+3))
-        else:
-          view.replace(edit, region, '<'+source+'></'+source.split(' ')[0]+'>')
-          new_selections.append(sublime.Region(region.end()+2, region.end()+2))
+	def run(self, edit):
+		view = self.view
+		self_closing_tags = re.compile("^(area|base|br|col|frame|hr|img|input|link|meta|param|command|embed|source)", re.I)
+		new_selections = []
+		for region in view.sel():
+			source = view.substr(region)
+			if not source.strip():
+				region = view.word(region)
+				source = view.substr(region)
+			if not source.strip():
+				pass
+			else:
+				if re.match("^\s", source):
+					view.replace(edit, region, '<p>'+source+'</p>')
+					new_selections.append(sublime.Region(region.end()+3, region.end()+3))
+				elif self_closing_tags.match(source):
+					view.replace(edit, region, '<'+source+'/>')
+					new_selections.append(sublime.Region(region.end()+3, region.end()+3))
+				else:
+					view.replace(edit, region, '<'+source+'></'+source.split(' ')[0]+'>')
+					new_selections.append(sublime.Region(region.end()+2, region.end()+2))
 
-    view.sel().clear()
-    for sel in new_selections:
-      view.sel().add(sel)
+		view.sel().clear()
+		for sel in new_selections:
+			view.sel().add(sel)
