8bc2f7586fd0343abcbf9060dcbf6c0ed5982923
Tito Bouzout <tito.bouzout@gmail.com>
2012-03-09 03:44:25 -0300

    Improves handling of XML tags

--- Tag.py
+++ Tag.py
@@ -7,23 +7,27 @@ class Tag():
 		Tag.regexp_is_valid								= re.compile("^[a-z0-9\:-_]+$", re.I);
 		Tag.regexp_self_closing_optional 	= re.compile("^<?(\?xml|\!|area|base|br|col|frame|hr|img|input|link|meta|param|command|embed|source|/?li|/?p)[^a-z]", re.I);
 		Tag.regexp_self_closing 					= re.compile("^<?(\?xml|\!|area|base|br|col|frame|hr|img|input|link|meta|param|command|embed|source)[^a-z]", re.I);
+		Tag.regexp_self_closing_xml   		= re.compile("^<?(\?xml|\!)[^a-z]", re.I);
 		Tag.regexp_is_closing 						= re.compile("^<?[^><]+/>", re.I);
 
 	def is_valid(self, content):
 		return Tag.regexp_is_valid.match(content)
 
-	def is_self_closing(self, content, ignore_optional = True):
+	def is_self_closing(self, content, ignore_optional = True, is_xml= False):
 		if ignore_optional:
 			return Tag.regexp_self_closing.match(content)
 		else:
-			return Tag.regexp_self_closing_optional.match(content) or Tag.regexp_is_closing.match(content)
+			if is_xml == False:
+				return Tag.regexp_self_closing_optional.match(content) or Tag.regexp_is_closing.match(content)
+			else:
+				return Tag.regexp_is_closing.match(content) or Tag.regexp_self_closing_xml.match(content)
 
-	def name(self, content, ignore_optional = True):
+	def name(self, content, ignore_optional = True, is_xml = False):
 		if content[:1] == '/':
 			tag_name = content.split('/')[1].split('>')[0];
 		else:
 			tag_name = content.split(' ')[0].split('>')[0];
-		if self.is_valid(tag_name) and not self.is_self_closing(content, ignore_optional):
+		if self.is_valid(tag_name) and not self.is_self_closing(content, ignore_optional, is_xml):
 			return tag_name
 		else:
 			return ''
